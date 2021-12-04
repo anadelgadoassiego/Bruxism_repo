80ec80ee37d45e51d25bb5512d3ac6e4b6f5a2c4
anadelgadoassiego <a.delgado37@usp.ceu.es>
2021-12-04 15:20:38 +0100

    cambios server ana

--- src/ui/ServerThreadsClient.java
+++ src/ui/ServerThreadsClient.java
@@ -36,58 +36,57 @@ public class ServerThreadsClient implements Runnable {
         
         inputStream = null;
         outputStream = null;
+        
         try {
             inputStream = socket.getInputStream();
             din= new DataInputStream(inputStream);
             outputStream = socket.getOutputStream();
             don = new DataOutputStream(outputStream);
-            int choice= din.readInt();
-            System.out.println(choice);
-            switch (choice) {
-                case 1:
-                   String response = din.readUTF();
+            while(true){
+                int choice= din.readInt();
+                System.out.println(choice);
+                switch (choice) {
+                    case 1:
+                       String response = din.readUTF();
 
-                    try {
-                        ui.Main.newUserPatient(response);
-                    } catch (Exception ex) {
-                        Logger.getLogger(ServerThreadsClient.class.getName()).log(Level.SEVERE, null, ex);
-                    }
+                        try {
+                            ui.Main.newUserPatient(response);
+                        } catch (Exception ex) {
+                            Logger.getLogger(ServerThreadsClient.class.getName()).log(Level.SEVERE, null, ex);
+                        }
 
-                    break;
-                case 2:
-                    String response_login = din.readUTF();
-                    System.out.println(response_login);
-                    String okay = "";
-                    try {
-                        okay = ui.Main.login(response_login);
-                        System.out.println(okay);
-                    } catch (Exception ex) {
-                        Logger.getLogger(ServerThreadsClient.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                    if ((okay == "Wrong credentials, please try again!") || (okay == "Invalid role")) {
-                        don.writeUTF(okay);
-                    } // We check the role
-                    else if (okay == "Welcome patient !" ) {
-                       don.writeUTF(okay);
-                       menuPatient();
-                    }
-                    break;
-                case 0:
-                    dbManager.disconnect();
-                    userManager.disconnect();
-                    return;
-                default:
-                    break;
+                        break;
+                    case 2:
+                        String response_login = din.readUTF();
+                        System.out.println(response_login);
+                        String okay = "";
+                        try {
+                            okay = ui.Main.login(response_login);
+                            System.out.println(okay);
+                        } catch (Exception ex) {
+                            Logger.getLogger(ServerThreadsClient.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        if ((okay == "Wrong credentials, please try again!") || (okay == "Invalid role")) {
+                            don.writeUTF(okay);
+                        } // We check the role
+                        else if (okay == "Welcome patient !" ) {
+                           don.writeUTF(okay);
+                           menuPatient();
+                        }
+                        break;
+                    case 0:
+                        dbManager.disconnect();
+                        userManager.disconnect();
+                        return;
+                    default:
+                        break;
+                }
             }
-            //We read until is finished the connection or character 'x'
-            while (((byteRead = inputStream.read()) != -1) || (byteRead != 'x')) {
-                caracter = (char) byteRead;
-
-            }
+            
         } catch (IOException ex) {
             Logger.getLogger(ServerThreadsClient.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
-            System.out.println("Character reception finished");
+            System.out.println("Character reception finished 1");
             releaseResourcesClient(inputStream, socket);
         }
         
@@ -102,68 +101,70 @@ public class ServerThreadsClient implements Runnable {
             din= new DataInputStream(inputStream);
             outputStream = socket.getOutputStream();
             don = new DataOutputStream(outputStream);
-            int choice= din.readInt();
-            System.out.println(choice);
-            switch (choice) {
-                case 1:
-                   String response_form = din.readUTF();
-                   String okay="";
+            while(true) {
+                int choice= din.readInt();
+                System.out.println(choice);
+                switch (choice) {
+                    case 1:
+                       String response_form = din.readUTF();
+                       String okay="";
 
-                    try {
-                        okay= ui.Main.completeForm(response_form);
-                    } catch (Exception ex) {
-                        Logger.getLogger(ServerThreadsClient.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                    if ((okay == "There was an error while saving") || (okay == "")) {
+                        try {
+                            okay= ui.Main.completeForm(response_form);
+                        } catch (Exception ex) {
+                            Logger.getLogger(ServerThreadsClient.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        if ((okay == "There was an error while saving") || (okay == "")) {
 
-                    } // We check the role
-                    else if (okay == "Form saved successfully" ) {
-                       don.writeUTF(okay);
-                    }
-                    break;
-                case 2:
-                    String response_login = din.readUTF();
-                    okay = "";
-                    try {
-                        okay = ui.Main.login(response_login);
-                    } catch (Exception ex) {
-                        Logger.getLogger(ServerThreadsClient.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                    if ((okay == "Wrong credentials, please try again!") || (okay == "Invalid role")) {
+                        } // We check the role
+                        else if (okay == "Form saved successfully" ) {
+                           don.writeUTF(okay);
+                        }
+                        break;
+                    case 2:
+                        String response_login = din.readUTF();
+                        okay = "";
+                        try {
+                            okay = ui.Main.login(response_login);
+                        } catch (Exception ex) {
+                            Logger.getLogger(ServerThreadsClient.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        if ((okay == "Wrong credentials, please try again!") || (okay == "Invalid role")) {
 
-                    } // We check the role
-                    else if (okay == "Welcome patient !" ) {
-                       don.writeUTF(okay);
+                        } // We check the role
+                        else if (okay == "Welcome patient !" ) {
+                           don.writeUTF(okay);
 
-                       menuPatient();
+                           menuPatient();
 
-                    }
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    String newName = din.readUTF();
-                    /*try {
-                        okay = ui.Main.changeUsername(newName);
-                    } catch (Exception ex) {
-                        Logger.getLogger(ServerThreadsClient.class.getName()).log(Level.SEVERE, null, ex);
-                    }*/
-                    break;
-                case 6:
-                    String newPassword = din.readUTF();
-                    /*try {
-                        okay = ui.Main.changePassword(newPassword);
-                    } catch (Exception ex) {
-                        Logger.getLogger(ServerThreadsClient.class.getName()).log(Level.SEVERE, null, ex);
-                    }*/
-                    break;
-                case 7:
-                    
-                    return;
-                default:
-                    break;
+                        }
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        String newName = din.readUTF();
+                        /*try {
+                            okay = ui.Main.changeUsername(newName);
+                        } catch (Exception ex) {
+                            Logger.getLogger(ServerThreadsClient.class.getName()).log(Level.SEVERE, null, ex);
+                        }*/
+                        break;
+                    case 6:
+                        String newPassword = din.readUTF();
+                        /*try {
+                            okay = ui.Main.changePassword(newPassword);
+                        } catch (Exception ex) {
+                            Logger.getLogger(ServerThreadsClient.class.getName()).log(Level.SEVERE, null, ex);
+                        }*/
+                        break;
+                    case 7:
+                        //releaseResourcesClient(inputStream, outputStream, din, don);
+                        return;
+                    default:
+                        break;
+                }
             }
         } catch (IOException ex) {
             Logger.getLogger(ServerThreadsClient.class.getName()).log(Level.SEVERE, null, ex);
@@ -187,4 +188,31 @@ public class ServerThreadsClient implements Runnable {
             Logger.getLogger(ServerReceiveCharactersViaNetwork.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
+    private static void releaseResourcesClient(InputStream inputStream, OutputStream outputStream, DataInputStream din, DataOutputStream don) {
+        try {
+            inputStream.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ServerReceiveCharactersViaNetwork.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        try {
+            outputStream.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ServerReceiveCharactersViaNetwork.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        try {
+            din.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ServerReceiveCharactersViaNetwork.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        try {
+            don.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ServerReceiveCharactersViaNetwork.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
 }
